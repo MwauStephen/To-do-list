bdeff99b3059fc8c4487d96981dbbc0e775a6f63
Mwau stephen <stephenmwau41@gmail.com>
2022-06-14 02:28:19 +0800

    added a submit handler

--- src/Components/NewTodo.js
+++ src/Components/NewTodo.js
@@ -1,8 +1,13 @@
 import React from "react";
+import styles from "./NewTodo.module.css";
 
 const NewTodo = () => {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form className={styles.form} onSubmit={submitHandler}>
       <label htmlFor="text">Task:</label>
       <input type="text" id="text" />
       <button>Add Task</button>

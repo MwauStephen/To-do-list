8734b3662e7e84f355875ffd71672f7d6757ea1d
Mwau stephen <stephenmwau41@gmail.com>
2022-06-13 19:35:52 +0800

    added a todo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Todo from "./Components/Todo";
 
 function App() {
-  return <div>Welcome to react js stephen</div>;
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <ul>
+      <li>Learn react js</li>
+      <li>Learn react js</li>
+      <li>Learn react js</li>
+      <li>Learn react js</li>
+    </ul>
+  );
+};
+
+export default Todo;

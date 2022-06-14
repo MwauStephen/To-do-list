fcb8f0fa7141ebacf3bc5e434fe5b1cd0ba7bc39
Mwau stephen <stephenmwau41@gmail.com>
2022-06-14 15:36:57 +0800

    imported the list component

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,1 +1,16 @@
+import React from "react";
+import TodoItem from "./TodoItem";
 
+const Todo = (props) => {
+  const items = ["Node-js", "php", "react-js"];
+
+  return (
+    <ul>
+      {items.map((item) => {
+        return <TodoItem text={item} />;
+      })}
+    </ul>
+  );
+};
+
+export default Todo;

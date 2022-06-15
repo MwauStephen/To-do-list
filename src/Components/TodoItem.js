ca4c8db78699c3088edae41758371cfc986fd492
Mwau stephen <stephenmwau41@gmail.com>
2022-06-16 00:27:47 +0800

    added a bind method on the remove item function

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./TodoItem.module.css";
 
 const TodoItem = (props) => {
-  return <li className={styles.li}>{props.text}</li>;
+  return (
+    <li className={styles.li} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
